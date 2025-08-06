2de51964ce7e71f9a1a1cd73f89a52468f39425b
Ajdin Mehmedović <ayden.diagrampixel@gmail.com>
2025-08-06 11:30:51 +0200

    particles completed

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -23,24 +23,30 @@ const particleTexture = textureLoader.load("/textures/particles/2.png");
 // Particles
 // Geometry
 const particlesGeometry = new THREE.BufferGeometry(1, 32, 32);
-const count = 5000;
+const count = 20000;
 const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
 for (let i = 0; i < count * 3; i++) {
   positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
 }
 particlesGeometry.setAttribute(
   "position",
   new THREE.BufferAttribute(positions, 3)
 );
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 // Material
 const particlesMaterial = new THREE.PointsMaterial();
 particlesMaterial.transparent = true;
 particlesMaterial.alphaMap = particleTexture;
-// particlesMaterial.alphaTest = 0.001;
 particlesMaterial.depthTest = false;
+particlesMaterial.vertexColors = true;
+// particlesMaterial.alphaTest = 0.001;
+/* particlesMaterial.depthTest = false;/*  */
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending;
 particlesMaterial.size = 0.1;
 particlesMaterial.sizeAttenuation = true;
-particlesMaterial.color = new THREE.Color("#ff88cc");
 particlesMaterial.map = particleTexture;
 
 // Points
@@ -111,7 +117,16 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
+  // Update particles
+  /* particles.rotation.y = elapsedTime * 0.2; */
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+    const x = particlesGeometry.attributes.position.array[i3 + 0];
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
+      elapsedTime + x
+    );
+  }
+  particlesGeometry.attributes.position.needsUpdate = true;
   // Update controls
   controls.update();
 
